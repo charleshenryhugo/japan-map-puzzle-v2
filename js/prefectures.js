1de8a7a2633a4351124f5463aba3e6c642291787
yue.zhu <yue.zhu@rakuten.com>
2021-04-20 14:08:07 +0900

    first commit

---
+++ js/prefectures.js
@@ -0,0 +1,66 @@
+const prefectures = [
+  // #FF8585 => #FFE65E
+  'hokkaido',
+
+  // #FFFB91
+  'aomori',
+  'akita',
+  'iwate',
+  'yamagata',
+  'miyagi',
+  'fukushima',
+
+  // #8BFFE8
+  'aichi',
+  'shizuoka',
+  'niigata',
+  'nagano',
+  'ishikawa',
+  'toyama',
+  'fukui',
+  'gifu',
+  'yamanashi',
+
+  // #79ff76
+  'ibaraki',
+  'chiba',
+  'tokyo',
+  'kanagawa',
+  'tochigi',
+  'gunma',
+  'saitama',
+
+  // #7272FF
+  'osaka',
+  'hyogo',
+  'wakayama',
+  'mie',
+  'kyoto',
+  'shiga',
+  'nara',
+
+  // #FF9141
+  'tottori',
+  'shimane',
+  'yamaguchi',
+  'hiroshima',
+  'okayama',
+
+  // #D16EFF
+  'kagawa',
+  'kochi',
+  'ehime',
+  'tokushima',
+
+  // #C5C5C5 =>
+  'oita',
+  'miyazaki',
+  'fukuoka',
+  'saga',
+  'kumamoto',
+  'nagasaki',
+  'kagoshima',
+  'okinawa-kagoshima',
+];
+
+export default prefectures;
---
+++ js/script.js
@@ -0,0 +1,216 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import prefectures from './prefectures.js';
+import prefectureSvgs from '../mapResources/*.svg';
+
+const mapContainer = document.querySelector('.map-container');
+const whiteMap = mapContainer.querySelector('svg');
+const whiteMapInnerFrame = whiteMap.getElementById('inner-frame');
+const whiteMapOuterFrame = whiteMap.getElementById('outer-frame');
+const puzzlePiecesBox = document.querySelector('.puzzle-pieces');
+
+const timer = document.querySelector('.timer');
+const timerText = timer.querySelector('.timer--text');
+const btnGiveUp = timer.querySelector('.btn--give-up');
+
+const controlBox = document.querySelector('.control-box');
+const btnStart = controlBox.querySelector('.btn--start');
+
+const [EASY, MEDIUM, HARD, EXPERT] = [0, 1, 2, 3];
+let gameMode = EASY;
+let matchedPieces = 0;
+
+// switch game mode on click
+controlBox.addEventListener('click', e => {
+  const target = e.target.closest('.btn[data-mode]');
+  if (!target) return;
+
+  controlBox
+    .querySelector(`.btn[data-mode="${gameMode}"]`)
+    .classList.remove('btn--selected');
+  target.classList.add('btn--selected');
+  gameMode = +target.dataset.mode;
+});
+
+/**
+ * get target bounding client rect in the map frame
+ * @param prefecture
+ * @returns {DOMRect}
+ * @private
+ */
+const getTargetClientRect = function (prefecture) {
+  return whiteMap.getElementById(prefecture).getBoundingClientRect();
+};
+
+/**
+ * create a puzzle piece element with the expected width
+ * @param prefecture prefecture name, e.g "hokkaido"
+ * @param prefectureNameHints whether to display prefecture name
+ * @returns {HTMLImageElement}
+ */
+const makePuzzlePiece = function (prefecture, prefectureNameHints = true) {
+  const targetRect = getTargetClientRect(prefecture);
+  const srcFileNameSuffix = prefectureNameHints ? '-jp' : '';
+
+  const piece = document.createElement('img');
+  piece.dataset.prefecture = prefecture;
+  piece.src = prefectureSvgs[`${prefecture}${srcFileNameSuffix}`];
+
+  piece.style.width = Number.parseFloat(targetRect.width) + 'px';
+
+  // position the piece center to [clientX, clientY]
+  piece.moveTo = function (clientX, clientY) {
+    const clientRect = this.getBoundingClientRect();
+    this.style.left =
+      clientX - clientRect.width / 2 + window.pageXOffset + 'px';
+    this.style.top =
+      clientY - clientRect.height / 2 + window.pageYOffset + 'px';
+  };
+
+  // check whether a piece has arrived its expected position
+  piece.arrived = function (sensitivity = 30) {
+    const clientRect = this.getBoundingClientRect();
+    const targetRect = getTargetClientRect(prefecture);
+    return (
+      clientRect.x >= targetRect.x - sensitivity &&
+      clientRect.x <= targetRect.x + sensitivity &&
+      clientRect.y <= targetRect.y + sensitivity &&
+      clientRect.y >= targetRect.y - sensitivity
+    );
+  };
+
+  // directly match a piece to its expected position
+  piece.matchToTarget = function () {
+    piece.style.top =
+      Number.parseFloat(targetRect.y) + mapContainer.scrollTop + 'px';
+    piece.style.left =
+      Number.parseFloat(targetRect.x) + mapContainer.scrollLeft + 'px';
+
+    piece.className = 'puzzle-piece--matched';
+  };
+
+  return piece;
+};
+
+const makePuzzlePiecesDraggable = function () {
+  puzzlePiecesBox.onmousedown = function (e) {
+    e.preventDefault();
+
+    const piece = e.target.closest('.puzzle-piece--initial');
+    if (!piece) return;
+
+    const clientRect = piece.getBoundingClientRect();
+    const shiftX = e.clientX - clientRect.left - clientRect.width / 2;
+    const shiftY = e.clientY - clientRect.top - clientRect.height / 2;
+
+    const pieceClone = makePuzzlePiece(
+      piece.dataset.prefecture,
+      gameMode === EASY
+    );
+    pieceClone.className = 'puzzle-piece--dragged';
+    pieceClone.style.left = Number.parseFloat(clientRect.x) + 'px';
+    pieceClone.style.top = Number.parseFloat(clientRect.y) + 'px';
+    mapContainer.append(pieceClone);
+    piece.classList.add('transparent');
+
+    pieceClone.moveTo(e.clientX - shiftX, e.clientY - shiftY);
+    const onMouseMove = function (e) {
+      pieceClone.moveTo(e.clientX - shiftX, e.clientY - shiftY);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+
+    document.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.onmouseup = null;
+
+      if (pieceClone.arrived(30)) {
+        pieceClone.matchToTarget();
+        piece.remove();
+        matchedPieces++;
+      } else {
+        pieceClone.remove();
+        piece.classList.remove('transparent');
+      }
+
+      if (matchedPieces >= prefectures.length) {
+        document.querySelector('.timer--text').textContent += ' 🥳COMPLETED!';
+      }
+    };
+  };
+};
+
+/**
+ * init map container
+ * clean existing puzzle pieces and set inner frame
+ */
+const initMapContainer = function () {
+  mapContainer
+    .querySelectorAll('[data-prefecture]')
+    .forEach(piece => piece.remove());
+
+  if (gameMode === HARD) {
+    whiteMapInnerFrame.style.opacity = 0;
+    return;
+  }
+
+  if (gameMode === EXPERT) {
+    whiteMapInnerFrame.style.opacity = 0;
+    whiteMapOuterFrame.style.opacity = 0;
+  }
+};
+
+/**
+ * create and randomly place all puzzle pieces as child elements inside the puzzlePiecesBox
+ * @param prefectureNameHints
+ */
+const initPuzzlePieces = function (prefectureNameHints = gameMode === EASY) {
+  while (puzzlePiecesBox.firstElementChild)
+    puzzlePiecesBox.lastElementChild.remove();
+
+  prefectures
+    .sort(() => 0.5 - Math.random())
+    .forEach(prefecture => {
+      const piece = makePuzzlePiece(prefecture, prefectureNameHints);
+      piece.className = 'puzzle-piece--initial';
+      puzzlePiecesBox.append(piece);
+    });
+};
+
+/**
+ * match all puzzle pieces to the map container
+ */
+const matchAllPuzzlePieces = function () {
+  prefectures.forEach(prefecture => {
+    const piece = makePuzzlePiece(prefecture, true);
+    mapContainer.append(piece);
+    piece.matchToTarget();
+  });
+};
+
+const initGame = function () {
+  timer.classList.add('hidden');
+  puzzlePiecesBox.classList.add('hidden');
+  controlBox.classList.remove('hidden');
+
+  whiteMapInnerFrame.style.opacity = 1;
+  whiteMapOuterFrame.style.opacity = 1;
+
+  matchedPieces = 0;
+
+  matchAllPuzzlePieces();
+  makePuzzlePiecesDraggable();
+};
+
+const startGame = function () {
+  timer.classList.remove('hidden');
+  puzzlePiecesBox.classList.remove('hidden');
+  controlBox.classList.add('hidden');
+
+  initMapContainer();
+  initPuzzlePieces();
+};
+
+initGame();
+
+btnStart.addEventListener('click', startGame);
+btnGiveUp.addEventListener('click', initGame);
